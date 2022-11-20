e8c1c1f188c36a760520b833bce1464e5b6d9630
hrprem16 <83371230+hrprem16@users.noreply.github.com>
2022-11-20 09:11:47 +0530

    Create SquareRoot(Integral).cpp

---
+++ Lecture 6: Operators and For Loop/Assignment/SquareRoot(Integral).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int m;
+    int n=1;
+    cin>>m;
+    int count=0;
+    while(n*n<=m){
+        count++;
+        n++;
+         
+    }
+    cout<<count;
+   
+   
+	
+}
